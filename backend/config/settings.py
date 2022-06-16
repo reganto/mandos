b8322a9f6d1946def5e5f7f33e4c75870023a495
Reganto <tell.reganto@gmail.com>
2022-06-16 19:28:43 +0430

    Added django cors settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3-rd Party
     "rest_framework",
+    "corsheaders",
     "django_ptpython",
     # Local
     "api.apps.ApiConfig",
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -127,3 +129,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Django CORS Headers stuff
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
